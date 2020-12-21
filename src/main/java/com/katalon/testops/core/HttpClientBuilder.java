dfe5bb096c9f0d0cfc13e2e5dbdf045e372157a3
sonhuynhpham <sonhuynhpham@kms-technology.com>
2020-12-21 16:58:52 +0700

    Remove generated api client

--- src/main/java/com/katalon/testops/core/HttpClientBuilder.java
+++ src/main/java/com/katalon/testops/core/HttpClientBuilder.java
@@ -1,16 +1,8 @@
 package com.katalon.testops.core;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.threetenbp.ThreeTenModule;
-import com.katalon.testops.core.web.CustomInstantDeserializer;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-import org.threeten.bp.Instant;
-import org.threeten.bp.OffsetDateTime;
-import org.threeten.bp.ZonedDateTime;
 
 public class HttpClientBuilder {
 
@@ -47,16 +39,6 @@ public class HttpClientBuilder {
 
     public RestTemplate build() {
         RestTemplate restTemplate = new RestTemplate();
-        for (HttpMessageConverter<?> converter : restTemplate.getMessageConverters()) {
-            if (converter instanceof AbstractJackson2HttpMessageConverter) {
-                ObjectMapper mapper = ((AbstractJackson2HttpMessageConverter) converter).getObjectMapper();
-                ThreeTenModule module = new ThreeTenModule();
-                module.addDeserializer(Instant.class, CustomInstantDeserializer.INSTANT);
-                module.addDeserializer(OffsetDateTime.class, CustomInstantDeserializer.OFFSET_DATE_TIME);
-                module.addDeserializer(ZonedDateTime.class, CustomInstantDeserializer.ZONED_DATE_TIME);
-                mapper.registerModule(module);
-            }
-        }
         // This allows us to read the response more than once - Necessary for debugging.
         SimpleClientHttpRequestFactory simpleRequestFactory = new SimpleClientHttpRequestFactory();
         simpleRequestFactory.setConnectTimeout(connectTimeout);
