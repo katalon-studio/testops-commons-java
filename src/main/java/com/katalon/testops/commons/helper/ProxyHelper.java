c2ef110d68e13c80d6ffe52d283899e2991fffa9
sonhuynhpham <sonhuynhpham@kms-technology.com>
2021-01-05 12:35:43 +0700

    Update pom

--- src/main/java/com/katalon/testops/commons/helper/ProxyHelper.java
+++ src/main/java/com/katalon/testops/commons/helper/ProxyHelper.java
@@ -1,6 +1,7 @@
 package com.katalon.testops.commons.helper;
 
 import com.github.markusbernhardt.proxy.ProxySearch;
+import com.github.markusbernhardt.proxy.selector.misc.BufferedProxySelector;
 import com.github.markusbernhardt.proxy.util.PlatformUtil;
 
 import java.net.ProxySelector;
@@ -27,7 +28,7 @@ public class ProxyHelper {
             proxySearch.addStrategy(ProxySearch.Strategy.FIREFOX);
         }
         // Cache 50 hosts for up to 30 minutes.
-        proxySearch.setPacCacheSettings(50, TimeUnit.MINUTES.toMillis(30));
+        proxySearch.setPacCacheSettings(50, TimeUnit.MINUTES.toMillis(30), BufferedProxySelector.CacheScope.CACHE_SCOPE_URL);
         return proxySearch;
     }
 }
