4d43cec9166cef425ea7b4554c4b3ba64e7c7ad7
Qui Le <qui.le@katalon.com>
2021-01-12 11:03:54 +0700

    update

--- src/main/java/com/katalon/testops/commons/core/Constants.java
+++ src/main/java/com/katalon/testops/commons/core/Constants.java
@@ -2,39 +2,39 @@ package com.katalon.testops.commons.core;
 
 public class Constants {
 
+    public static final String TESTOPS_SERVER_URL_DEFAULT = "https://testops.katalon.io";
+
+    public static final String TESTOPS_REPORT_DIRECTORY_DEFAULT = "target/testops-report";
+
     public static final String REPORT_FILE_EXTENSION = ".json";
 
     public static final String TESTOPS_UUID_ATTR = "TO_UUID";
 
-    public static final String TESTOPS_REPORT_DIRECTORY = "testops.reportFolder";
+    public static final String TESTOPS_SESSION_ID = "testops.session-id";
 
-    public static final String TESTOPS_REPORT_DIRECTORY_DEFAULT = "target/testops-report";
+    public static final String TESTOPS_REPORT_DIRECTORY = "testops.report-folder";
 
-    public static final String TESTOPS_PROJECT_ID = "testops.projectId";
+    public static final String TESTOPS_PROJECT_ID = "testops.project-id";
 
-    public static final String TESTOPS_SERVER_URL = "testops.serverUrl";
+    public static final String TESTOPS_SERVER_URL = "testops.server-url";
 
-    public static final String TESTOPS_BUILD_LABEL = "testops.buildLabel";
+    public static final String TESTOPS_BUILD_LABEL = "testops.build-label";
 
-    public static final String TESTOPS_BUILD_URL = "testops.buildUrl";
+    public static final String TESTOPS_BUILD_URL = "testops.build-url";
 
-    public static final String TESTOPS_SERVER_URL_DEFAULT = "https://testops.katalon.io";
-
-    public static final String TESTOPS_API_KEY = "testops.apiKey";
-
-    public static final String REPORT_PATTERN = ".*(?i)\\.json";
+    public static final String TESTOPS_API_KEY = "testops.api-key";
 
     public static final String TESTOPS_PROXY_OPTION = "testops.proxy.option";
 
-    public static final String TESTOPS_PROXY_SERVER_TYPE = "testops.proxy.serverType";
+    public static final String TESTOPS_PROXY_SERVER_TYPE = "testops.proxy.server-type";
 
-    public static final String TESTOPS_PROXY_HOST = "testops.proxy.serverAddress";
+    public static final String TESTOPS_PROXY_HOST = "testops.proxy.serve-address";
 
-    public static final String TESTOPS_PROXY_PORT = "testops.proxy.serverPort";
+    public static final String TESTOPS_PROXY_PORT = "testops.proxy.server-port";
 
     public static final String TESTOPS_PROXY_USERNAME = "testops.proxy.username";
 
     public static final String TESTOPS_PROXY_PASSWORD = "testops.proxy.password";
 
-    public static final String TESTOPS_SESSION_ID = "testops.sessionId";
+    public static final String REPORT_PATTERN = ".*(?i)\\.json";
 }
